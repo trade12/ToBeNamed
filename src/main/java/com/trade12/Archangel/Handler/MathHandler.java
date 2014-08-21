e1b7c95fa35d4dee3dbb83dd74eef37232af97f3
trade12 <kieranma@hotmail.com>
2014-08-21 17:49:28 +0100

    no message

---
+++ src/main/java/com/trade12/Archangel/Handler/MathHandler.java
@@ -0,0 +1,40 @@
+package com.trade12.Archangel.Handler;
+
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+
+/**
+ * Created by kieran on 21/08/2014.
+ */
+public class MathHandler {
+
+    public static boolean isDaytime(World world, int x, int y, int z)
+    {
+        int a = world.getSavedLightValue(EnumSkyBlock.Sky, x,y,z) - world.skylightSubtracted;
+        float f = world.getCelestialAngleRadians(1.0F);
+
+        if (f < (float)Math.PI)
+        {
+            f += (0.0F - f) * 0.2F;
+        }
+        else
+        {
+            f += (((float)Math.PI * 2F) -f) *0.2F;
+        }
+
+        a = Math.round((float)a* MathHelper.cos(f));
+
+        if (a < 0)
+        {
+            a = 0;
+        }
+        if (a > 15)
+        {
+            a = 15;
+        }
+
+        return a != 0;
+
+    }
+}
--- src/main/java/com/trade12/Archangel/Items/Ariel/ItemArielPower.java
+++ src/main/java/com/trade12/Archangel/Items/Ariel/ItemArielPower.java
@@ -1,8 +1,20 @@
 package com.trade12.Archangel.Items.Ariel;
 
 import com.trade12.Archangel.Archangel;
+import com.trade12.Archangel.Config.ConfigHandler;
+import com.trade12.Archangel.Handler.MathHandler;
 import com.trade12.Archangel.lib.Ref;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * Created by Kieran on 21/08/2014.
@@ -14,5 +26,44 @@ public class ItemArielPower extends Item {
         this.setMaxStackSize(1);
         this.setCreativeTab(Archangel.tabCustom);
         this.setUnlocalizedName(Ref.UNLOCALISED_ARIEL_POWER);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IIconRegister register)
+    {
+        this.itemIcon = register.registerIcon(Ref.MOD_ID + ":" + Ref.UNLOCALISED_ARIEL_POWER);
+    }
+
+    public void onCreated(ItemStack itemStack, World world, EntityPlayer player)
+    {
+        if (itemStack.stackTagCompound == null)
+            itemStack.setTagCompound(new NBTTagCompound());
+
+        itemStack.stackTagCompound.setInteger("Charge",0);
+    }
+
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List info, boolean useInfo)
+    {
+        if (itemStack.stackTagCompound == null)
+            itemStack.setTagCompound(new NBTTagCompound());
+
+        info.add("Current Charge: " + itemStack.stackTagCompound.getInteger("Charge"));
+        info.add("Portable Energy Source for your mystical  items!");
+    }
+
+    public void onUpdate(ItemStack itemStack, World world, Entity entity, int ia, boolean ba)
+    {
+        if (entity instanceof EntityPlayer)
+        {
+            if (itemStack.stackTagCompound == null)
+                itemStack.setTagCompound(new NBTTagCompound());
+
+            EntityPlayer player = (EntityPlayer) entity;
+            if (itemStack.stackTagCompound.getInteger("Charge") < ConfigHandler.maxCharge && player.worldObj.canBlockSeeTheSky((int) player.posX, (int) player.posY, (int) player.posZ) && MathHandler.isDaytime(player.worldObj, (int) player.posX, (int) player.posY, (int) player.posZ));
+            {
+                itemStack.stackTagCompound.setInteger("Charge",itemStack.stackTagCompound.getInteger("Charge") + 1);
+            }
+        }
     }
 }
