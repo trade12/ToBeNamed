f9223bfea98a3543b41ccb422d37f9c818261b13
Kieran Anson <kieranma@hotmail.com>
2014-08-26 19:58:08 +0100

    Whistle

--- src/main/java/com/trade12/Archangel/Items/ItemLoader.java
+++ src/main/java/com/trade12/Archangel/Items/ItemLoader.java
@@ -74,7 +74,7 @@ public class ItemLoader {
         angelPurity = new ItemAngelPurity();
         pureSeed = new ItemPureSeeds();
         pebbles = new ItemPebbles();
-       // angelWhistle = new AngelWhistle();
+        angelWhistle = new AngelWhistle();
         heart = new ItemHeart();
 
         guideBook = new ItemGuideBook();
@@ -116,7 +116,7 @@ public class ItemLoader {
         GameRegistry.registerItem(angelPurity, "Archangel_angelPurity");
         GameRegistry.registerItem(pureSeed, "Archangel_pureSeed");
         GameRegistry.registerItem(pebbles, "Archangel_Pebbles");
-    //    GameRegistry.registerItem(angelWhistle, "Archangel_Whistle");
+        GameRegistry.registerItem(angelWhistle, "Archangel_Whistle");
         GameRegistry.registerItem(heart, "Archangel_heart");
 
         GameRegistry.registerItem(guideBook, "Archangel_guideBook");
